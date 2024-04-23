36806378d1fe3c5c1a137110f400a17cdd42af47
Shampy <157746114+codershampy@users.noreply.github.com>
2024-04-23 23:58:25 +0530

    Delete Count array using sizeof operator.c

--- Count array using sizeof operator.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int arr[]={1,2,3,4,5};
-    int length = sizeof(arr)/sizeof(arr[0]);
-    printf("Number of elements present in given array:%d",length);
-    return 0;
-}
